676049afc65da78d85a2e391c6b590c7f6dde947
Kitsunegatari <andresfelipepinzoncamacho@gmail.com>
2024-10-04 10:18:27 -0500

    Se crea la funcion de multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,10 @@ public class Calculadora{
         this.x = x;
         this.y = y;
     }
+    public double multiplicacion(double x, double y){
+        resultado = x * y;
+        return resultado;
+    }
     public static void main(String[] args) {
         
     }
