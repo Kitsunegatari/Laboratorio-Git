9db0e6f48d8e5f2fb34311aa2e8a424513ec7860
Kitsunegatari <andresfelipepinzoncamacho@gmail.com>
2024-10-04 10:10:49 -0500

    Se crea estructura base de la clase calculadora ademas de la creacion de la rama dev

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora{
+    public double x,y,resultado;
+
+    public Calculadora(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
